1389d09e982c11108c22e3cb6ca5ce0c3b35f4d3
lei.lin <lei.lin@cosunet.com.cn>
2017-03-21 15:08:46 +0800

    弹出提示框优化

--- js/alert_msg/alert_msg.js
+++ js/alert_msg/alert_msg.js
@@ -10,5 +10,12 @@ function hideDialog1(){
 	$("#dialog1").yuerDialog("hide");
 }
 function showErroeDialog(){
-	$("#errorDialog").yuerMsgDialog("show","error","哈哈哈哈");
+	$("#errorDialog").yuerMsgDialog("show","error","哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈");
+	//$("#errorDialog").yuerMsgDialog("show","error","123456789012345678901234567890");
+}
+function showInfoDialog(){
+	$("#errorDialog").yuerMsgDialog("show","info","哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈");
+}
+function showConfirmDialog(){
+	$("#errorDialog").yuerMsgDialog("show","confirm","哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈");
 }
